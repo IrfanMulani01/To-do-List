f6f90a4ad68682add9ccd3f74714dd2221ace440
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-06 14:18:06 +0530

    create AddTasks model in fields title and status

--- to_do/task/models.py
+++ to_do/task/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class AddTasks(models.Model):
+    status_choice = [
+        ('complate', 'Complate'),
+        ('incomplate', 'Incomplate'),
+        ('high', 'High Priority'),
+    ]
+    title = models.CharField(max_length=50)
+    status = models.CharField(max_length=50, choices=status_choice, default='incomplate')
+
+
+    def __str__(self):
+        return self.title
