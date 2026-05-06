ad2fa495394299488dace18937567c917fa43231
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-06 20:19:15 +0530

    set path of four pages add, list, and update in app-based urls

---
+++ to_do/task/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.add_task),
+    path('update/', views.update_task, name='update'),
+    path('list/', views.list_task, name='list'),
+    path('delete/', views.delete_task, name='delete'),
+]
