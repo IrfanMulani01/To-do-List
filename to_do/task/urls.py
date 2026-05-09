70e7a4b0d2563e14c24327f0e0d5a7b0de0a3e2c
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-09 14:09:31 +0530

    set name to add_task path

--- to_do/task/urls.py
+++ to_do/task/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.add_task),
+    path('', views.add_task, name='add_task'),
     path('update/', views.update_task, name='update'),
     path('list/', views.list_task, name='list'),
     path('delete/', views.delete_task, name='delete'),
