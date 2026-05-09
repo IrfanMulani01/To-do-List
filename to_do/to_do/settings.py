f9a56253516d26fd8a16f88aded788cfd3992990
IrfanMulani01 <irfanmulani0931@gmail.com>
2026-05-09 13:48:10 +0530

    set path of static files

--- to_do/to_do/settings.py
+++ to_do/to_do/settings.py
@@ -116,5 +116,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_URL = [BASE_DIR / 'static']
+STATICFILES_URL = [BASE_DIR / 'static']
 
